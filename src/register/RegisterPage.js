fc052ba2800d4cb0f04055a92d4fab0b35825611
jason-json <jason.json@kakaocorp.com>
2021-04-07 18:41:28 +0900

    Test register event

--- src/register/RegisterPage.js
+++ src/register/RegisterPage.js
@@ -10,6 +10,11 @@ import { apiGet } from '../utils/apiUtils';
 const RegisterPage = props => {
   const [email, setEmail] = useState('')
   const [code, setCode] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [gender, setGender] = useState('')
+  const [grade, setGrade] = useState('')
+  const [major, setMajor] = useState('')
 
   const setValue = setter => e => setter(e.target.value)
 
@@ -23,6 +28,16 @@ const RegisterPage = props => {
     console.log(r)
   }
 
+  const onRegister = () => {
+    console.log(email)
+    console.log(code)
+    console.log(password)
+    console.log(passwordConfirm)
+    console.log(gender)
+    console.log(grade)
+    console.log(major)
+  }
+
   return (
     <Layout hasNavigation={false}>
       <main className="register">
@@ -34,15 +49,15 @@ const RegisterPage = props => {
           <TextInput className="input" placeholder="인증번호" onChange={setValue(setCode)} />
           <ButtonInput className="send" value="확인" onClick={onVerify} />
         </div>
-        <TextInput className="password" type="password" placeholder="***************" />
-        <TextInput className="password" type="password" placeholder="***************" />
+        <TextInput className="password" type="password" placeholder="***************" onChange={setValue(setPassword)}/>
+        <TextInput className="password" type="password" placeholder="***************" onChange={setValue(setPasswordConfirm)}/>
         <div className="info">
-          <SelectInput className="gender" >
+          <SelectInput className="gender" onChange={setValue(setGender)}>
             <option value="" disabled selected>성별</option>
             <option value="M">남자</option>
             <option value="W">여자</option>
           </SelectInput>
-          <SelectInput className="grade">
+          <SelectInput className="grade" onChange={setValue(setGrade)}>
             <option value="" disabled selected>학년</option>
             <option value="1">1학년</option>
             <option value="2">2학년</option>
@@ -53,7 +68,7 @@ const RegisterPage = props => {
           </SelectInput>
         </div>
         {/* TODO: 서버로 부터 받아야함  */}
-        <SelectInput className="major">
+        <SelectInput className="major" onChange={setValue(setMajor)}>
             <option value="" disabled selected>학과</option>
             <option value="1">컴퓨터공학과</option>
             <option value="2">간호학과</option>
@@ -62,7 +77,7 @@ const RegisterPage = props => {
             <option value="5">중어중문학과</option>
             <option value="6">연극영화과</option>
           </SelectInput>
-        <ButtonInput className="submit" value="가입하기" />
+        <ButtonInput className="submit" value="가입하기" onClick={onRegister}/>
       </main>
     </Layout>
   );
