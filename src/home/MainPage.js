5a2526faf52f81dd53a886a4b9e2d47e73288204
jason-json <jason.json@kakaocorp.com>
2021-05-01 02:16:23 +0900

    feature: WebRTC UI/func

--- src/home/MainPage.js
+++ src/home/MainPage.js
@@ -1,24 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import Circle from '../common/anime/Circle';
+
 import Layout from '../common/Layout';
-import { getRTC } from '../utils/rtcUtils';
+import WebRTC from '../common/WebRTC';
 import {ReactComponent as Logo} from '../images/logo.svg';
+import './MainPage.css'
+
+export const PEER_SEARCHING = 'PEER_SEARCHING'
+export const PEER_CONNECTED = 'PEER_CONNECTED'
+export const PEER_DISCONNECTED = 'PEER_DISCONNECTED'
 
 const MainPage = props => {
-  const [rtc, setRTC] = useState(null)
+  const [rtc, setRTC] = useState({})
+  const [isSearching, setIsSearching] = useState(false)
+  const audio = useRef(null)
+
+  window.rtc = rtc
 
   useEffect(()=>{
-    setRTC(getRTC())
+    setRTC(new WebRTC({
+      audioElement:audio.current,
+      onSearch: () => setIsSearching(true),
+      onMiss: () => setIsSearching(false),
+      onConnect: () => console.log('connect!'),
+      onDisconnect: () => setIsSearching(false)
+    }))
   }, [])
 
-  const onMatch = e => {
-    console.log('click')
-    rtc.offer()
+  const onSearch = e => {
+    rtc.createOffer()
+  }
+
+  const onPlay = () => {
+    audio.current.play()
   }
 
   return (
     <Layout>
-      <main className="login">
-        <Logo className="logo" onClick={onMatch}/>    
+      <main className="home">
+        <Circle className="search" onClick={onSearch} disabled={!isSearching}/>
+        {/* <Logo onClick={onPlay}/> */}
+        <audio ref={audio} />
       </main>
     </Layout>
   );
