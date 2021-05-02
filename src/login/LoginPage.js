102786a22ea78795ba216084e799eef8eeeefa28
jason-json <jason.json@kakaocorp.com>
2021-05-02 18:21:24 +0900

    chore: find password temp link

--- src/login/LoginPage.js
+++ src/login/LoginPage.js
@@ -50,7 +50,7 @@ const LoginPage = props => {
           <ButtonInput className="submit" type="submit" value="로그인" onClick={onLogin} onSubmit={onLogin}/>
         </form>
         <div className="help">
-          <Link className="password" onClick={onTest}>비밀번호찾기</Link>
+          <Link className="password" to="/login" onClick={onTest}>비밀번호찾기</Link>
           <div className="dividor">/</div>
           <Link className="register" to="/register">회원가입</Link>
         </div>
