6baa67f419a561a1f216f19316ea01c3b70e164d
jason-json <jason.json@kakaocorp.com>
2021-05-02 18:24:42 +0900

    feature: page auth

--- src/utils/hookUtils.js
+++ src/utils/hookUtils.js
@@ -1,8 +1,11 @@
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
+import { pushToast } from "../common/commonAction";
 
 export const useAuth = () => {
   const history = useHistory()
+  const dispatch = useDispatch()
 
   useEffect(() => {
     let cookieName = 'isLogin'
@@ -16,6 +19,7 @@ export const useAuth = () => {
 
     if (document.cookie.indexOf(cookieName + '=') !== -1) {
       history.push('/login')
+      dispatch(pushToast('로그인이 필요합니다.'))
     }
   },[history]) 
 }
