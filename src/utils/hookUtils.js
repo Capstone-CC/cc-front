0950dd63adf506f1c4e3a2d539816a653d826229
jason-json <jason.json@kakaocorp.com>
2021-05-03 01:37:15 +0900

    fix: login auth

--- src/utils/hookUtils.js
+++ src/utils/hookUtils.js
@@ -3,14 +3,14 @@ import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import { pushToast } from "../common/commonAction";
 
-const AUTH_COOKIE_NAME = '; isLogin'
+const AUTH_COOKIE_NAME = 'isLogin'
 
 export const useAuth = () => {
   const history = useHistory()
   const dispatch = useDispatch()
 
   useEffect(() => {
-    if (document.cookie.indexOf(AUTH_COOKIE_NAME) === -1) {
+    if (document.cookie.indexOf(`${AUTH_COOKIE_NAME}=true`) === -1) {
       history.push('/login')
       dispatch(pushToast('로그인이 필요합니다.'))
     }
@@ -18,11 +18,11 @@ export const useAuth = () => {
 }
 
 export const logoutCookie = () => {
-  document.cookie = document.cookie.replace(AUTH_COOKIE_NAME,'')
+  document.cookie = document.cookie.replace(`${AUTH_COOKIE_NAME}=true`,`${AUTH_COOKIE_NAME}=false`)
 }
 
 export const loginCookie = () => {
-  if(document.cookie.indexOf(AUTH_COOKIE_NAME) === -1){
-    document.cookie += AUTH_COOKIE_NAME
+  if(document.cookie.indexOf(`${AUTH_COOKIE_NAME}=true`) === -1){
+    document.cookie += `${AUTH_COOKIE_NAME}=true`
   }
 }
