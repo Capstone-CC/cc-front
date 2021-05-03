0ea221fef9d9dcde7aed49525048182d38adb9a0
jason-json <jason.json@kakaocorp.com>
2021-05-04 01:20:00 +0900

    add: matching phase

--- src/home/MainPage.js
+++ src/home/MainPage.js
@@ -1,41 +1,89 @@
 import React, { useEffect, useRef, useState } from 'react';
-import Circle from '../common/anime/Circle';
+import { useDispatch } from 'react-redux';
 
+import Circle, { CIRCLE_COLOR } from '../common/anime/Circle';
+import ButtonInput from '../common/input/ButtonInput';
+import { pushToast } from '../common/commonAction';
 import Layout from '../common/Layout';
 import WebRTC from '../common/WebRTC';
-import {ReactComponent as Logo} from '../images/logo.svg';
 import './MainPage.css'
 
-export const PEER_SEARCHING = 'PEER_SEARCHING'
-export const PEER_CONNECTED = 'PEER_CONNECTED'
-export const PEER_DISCONNECTED = 'PEER_DISCONNECTED'
+const MATCH_STATE = {
+  DISCONNECT: 0,
+  SEARCH: 1,
+  CONNECT: 2,
+}
+
+const circleColorMap = {
+  [MATCH_STATE.DISCONNECT]: CIRCLE_COLOR.BLACK,
+  [MATCH_STATE.SEARCH]: CIRCLE_COLOR.YELLOW,
+  [MATCH_STATE.CONNECT]: CIRCLE_COLOR.PINK,
+}
+
+const classNameMap = {
+  [MATCH_STATE.DISCONNECT]: 'default',
+  [MATCH_STATE.SEARCH]: 'search',
+  [MATCH_STATE.CONNECT]: 'connect',
+}
 
 const MainPage = props => {
-  const [rtc, setRTC] = useState({})
-  const [isSearching, setIsSearching] = useState(false)
+  const [searchState, setSearchState] = useState(MATCH_STATE.DISCONNECT)
+  const dispatch = useDispatch()
+  const rtc = useRef(null)
   const audio = useRef(null)
 
-  window.rtc = rtc
+  window.rtc = rtc.current
 
-  useEffect(()=>{
-    setRTC(new WebRTC({
-      audioElement:audio.current,
-      onSearch: () => setIsSearching(true),
-      onMiss: () => setIsSearching(false),
-      onConnect: () => console.log('connect!'),
-      onDisconnect: () => setIsSearching(false)
-    }))
+  useEffect(() => {
+
+    return () => {
+      onCancel()
+    }
   }, [])
 
   const onSearch = e => {
-    rtc.createOffer()
-    audio.current.play()
+    if(!rtc.current) {
+      rtc.current = new WebRTC({
+        audioElement:audio.current,
+        onSearch: () => {
+          setSearchState(MATCH_STATE.SEARCH)
+          dispatch(pushToast('매칭을 시작합니다.'))
+        },
+        onMiss: () => {
+          setSearchState(MATCH_STATE.DISCONNECT)
+          dispatch(pushToast('매칭 상대를 찾지 못했습니다.'))
+        },
+        onConnect: () => {
+          setSearchState(MATCH_STATE.CONNECT)
+          dispatch(pushToast('상대방과 매칭되었습니다!'))
+        },
+        onDisconnect: () => {
+          setSearchState(MATCH_STATE.DISCONNECT)
+          dispatch(pushToast('매칭을 종료합니다.'))
+        },
+      })
+    } else{
+      rtc.current.search()
+    } 
+  }
+
+  const onCancel = () => {
+    if(typeof rtc.current?.close === 'function') rtc.current.close()
   }
 
   return (
-    <Layout>
-      <main className="home">
-        <Circle className="search" onClick={onSearch} disabled={!isSearching}/>
+    <Layout hideNavigation={searchState !== MATCH_STATE.DISCONNECT}>
+      <main className={`home ${classNameMap[searchState]}`}>
+        <Circle className="start" onClick={onSearch} color={circleColorMap[searchState]} />
+        <div className="interface">
+          <div className="search">
+            <ButtonInput className="cancel" value="매칭 취소" onClick={onCancel} />
+          </div>
+          <div className="connect">
+            <ButtonInput className="disagree" value="거절 하기" onClick={onCancel} />
+            <ButtonInput className="agree" value="수락 하기" />
+          </div>
+        </div>
         <audio ref={audio} />
       </main>
     </Layout>
