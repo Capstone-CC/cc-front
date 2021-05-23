de56e632f738980654d01d346f5ec12b109baeb6
jason-json <jason.json@kakaocorp.com>
2021-05-23 16:02:12 +0900

    feature: chat room list tab

--- src/chat/ChatListPage.js
+++ src/chat/ChatListPage.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import Layout from '../common/Layout';
+import { apiGet } from '../utils/apiUtils'
+import ChatRoom from './ChatRoom';
+import './ChatListPage.css'
 
 const ChatListPage = props => {
+  const [chatList, setChatList] = useState([])
+
+  const getChatRoomList = async () => {
+    const r = await apiGet('/chatroom/list')
+
+    setChatList(r?.accountProfileApiResponse?.chatroomApiResponseList || [])
+    console.log(r?.accountProfileApiResponse?.chatroomApiResponseList.length)
+  }
+
+  useEffect(()=>{
+    getChatRoomList()
+  },[])
+
+  const onChatRoomEnter = id => e => {
+
+  }
 
   return (
     <Layout>
-      채팅
+      <main className="chat">
+        {chatList.map(({id, name, otherImg}) => (
+          <ChatRoom key={id} name={name} imageUrl={otherImg} onClick={onChatRoomEnter(id)} />
+        ))}
+      </main>
     </Layout>
   );
 };
---
+++ src/chat/ChatRoom.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import Profile from '../images/profile-default.png';
+import './ChatRoom.css'
+
+const ChatRoom = props => {
+  const {className, name, imageUrl, ...rest} = props
+
+  return (
+    <div {...rest} className={`chat-room ${className ? className : ''}`}>
+      <div className="option-left">
+
+      </div>
+      <div className="left">
+        <img src={imageUrl || Profile} alt="profile"/>
+      </div>
+      <div className="right">
+        <div className="name">
+          {name}
+        </div>
+        <div className="current">
+
+        </div>
+      </div>
+      <div className="option-right">
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default ChatRoom
